6aef634d27b786e1c874d0520a77dadc0d356404
QLSDesarrollo <QLSDesarrollo@QLSDesarrolloPC>
2022-10-12 20:19:14 -0600

    Carlos Arturo commit 12 OCT 2022 2019

--- pescolar-web/src/main/java/sv/gob/mined/pescolar/web/RubroPorCeView.java
+++ pescolar-web/src/main/java/sv/gob/mined/pescolar/web/RubroPorCeView.java
@@ -14,8 +14,11 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import lombok.Getter;
 import lombok.Setter;
+import sv.gob.mined.pescolar.model.Departamento;
+import sv.gob.mined.pescolar.model.Municipio;
 import sv.gob.mined.pescolar.model.ProcesoAdquisicion;
 import sv.gob.mined.pescolar.model.RubroPorCe;
 import sv.gob.mined.pescolar.model.RubrosAmostrarInteres;
@@ -37,6 +40,9 @@ public class RubroPorCeView implements Serializable {
 
     private String codigoEntidad;
     private String codigoRubro;
+    private String tiposeleccion;
+    private String iddepartamento;
+    private String idmunicipio;
 
     private Boolean deshabilitado = true;
 
@@ -46,6 +52,12 @@ public class RubroPorCeView implements Serializable {
 
     private List<RubrosAmostrarInteres> listrubrosamostrarinteres = new ArrayList();
     private List<RubroPorCe> listrubroporce = new ArrayList();
+    private List<RubroPorCe> listseleccionrubroporce = new ArrayList();
+
+    private List<VwCatalogoEntidadEducativa> listentidadeseducativas = new ArrayList();
+    private List<VwCatalogoEntidadEducativa> seleccionentidadEducativa = new ArrayList();
+    private List<Departamento> listdepartamento = new ArrayList();
+    private List<Municipio> listmunicipio = new ArrayList();
 
     @Inject
     private SessionView sessionView;
@@ -63,7 +75,16 @@ public class RubroPorCeView implements Serializable {
     public void init() {
         prepareEdit();
         listrubrosamostrarinteres = rubrosamostrarinteresrepo.findAll();
+        listentidadeseducativas.clear();
+        Query q = em.createQuery("select dep from Departamento dep order by dep.id", Departamento.class);
+        listdepartamento = q.getResultList();
+        listmunicipio.clear();
+        seleccionentidadEducativa.clear();
+        tiposeleccion = "0";
+        iddepartamento = "";
+        idmunicipio = "";
         rubroporce = null;
+        listseleccionrubroporce.clear();
     }
 
     public void prepareCreate() {
@@ -75,6 +96,14 @@ public class RubroPorCeView implements Serializable {
         setCodigoEntidad("");
         entidadEducativa = new VwCatalogoEntidadEducativa();
 
+        listentidadeseducativas.clear();
+        listmunicipio.clear();
+        seleccionentidadEducativa.clear();
+        tiposeleccion = "0";
+        iddepartamento = "";
+        idmunicipio = "";
+        listseleccionrubroporce.clear();
+
     }
 
     public void nuevo() {
@@ -83,6 +112,14 @@ public class RubroPorCeView implements Serializable {
         rubroporce = null;
         listrubroporce.clear();
         entidadEducativa = null;
+
+        listentidadeseducativas.clear();
+        listmunicipio.clear();
+        seleccionentidadEducativa.clear();
+        tiposeleccion = "0";
+        iddepartamento = "";
+        idmunicipio = "";
+        listseleccionrubroporce.clear();
     }
 
     public void buscarEntidadEducativa() {
@@ -95,7 +132,7 @@ public class RubroPorCeView implements Serializable {
             if (entidadEducativa == null) {
                 JsfUtil.mensajeAlerta("No se encuentra el centro educativo con código: " + getCodigoEntidad());
             } else {
-                listrubroporce = rubroporceRepo.listarnoborradosporce(codigoEntidad); 
+                listrubroporce = rubroporceRepo.listarnoborradosporce(codigoEntidad);
             }
             //} else {
             //JsfUtil.mensajeAlerta("Debe de seleccionar un proceso de adquisición.");
@@ -105,13 +142,113 @@ public class RubroPorCeView implements Serializable {
         }
     }
 
+    public void onSelectDepartamento() {
+        seleccionentidadEducativa.clear();
+        listmunicipio.clear();
+        listentidadeseducativas.clear();
+        listseleccionrubroporce.clear();
+
+        if (iddepartamento == null) {
+            iddepartamento = "";
+        }
+
+        if (iddepartamento.equals("")) {
+            idmunicipio = "";
+        } else {
+            Query q, q2;
+            if (iddepartamento.equals("00")) {
+                q = em.createQuery("select mun from Municipio mun order by mun.codigoDepartamento asc, mun.codigoMunicipio asc", Municipio.class);
+            } else {
+                q = em.createQuery("select mun from Municipio mun where mun.codigoDepartamento = '" + iddepartamento + "' order by mun.codigoDepartamento asc, mun.codigoMunicipio asc", Municipio.class);
+                q2 = em.createQuery("select vw from VwCatalogoEntidadEducativa vw where vw.codigoDepartamento = '" + iddepartamento + "' order by vw.codigoDepartamento asc, vw.codigoMunicipio asc, vw.codigoCanton asc", VwCatalogoEntidadEducativa.class);
+                listentidadeseducativas = q2.getResultList();
+            }
+            listmunicipio = q.getResultList();
+        }
+    }
+
+    public void onSelectMunicipio() {
+        seleccionentidadEducativa.clear();
+        listentidadeseducativas.clear();
+        listseleccionrubroporce.clear();
+
+        // System.out.println("idMunicipio:" + idmunicipio);
+        // System.out.println("select vw from VwCatalogoEntidadEducativa vw where vw.idMunicipio.id = " + idmunicipio + " order by vw.codigoDepartamento asc, vw.codigoMunicipio asc, vw.codigoCanton asc");
+        if (idmunicipio == null) {
+            idmunicipio = "";
+        }
+
+        if (idmunicipio.equals("")) {
+            if (iddepartamento == null) {
+                iddepartamento = "";
+            }
+
+            if (!iddepartamento.equals("00") && !iddepartamento.equals("")) {
+                Query q;
+                q = em.createQuery("select vw from VwCatalogoEntidadEducativa vw where vw.codigoDepartamento = '" + iddepartamento + "' order by vw.codigoDepartamento asc, vw.codigoMunicipio asc, vw.codigoCanton asc", VwCatalogoEntidadEducativa.class);
+                listentidadeseducativas = q.getResultList();
+            }
+
+        } else {
+            Query q;
+            q = em.createQuery("select vw from VwCatalogoEntidadEducativa vw where vw.idMunicipio.id = " + idmunicipio + " order by vw.codigoDepartamento asc, vw.codigoMunicipio asc, vw.codigoCanton asc", VwCatalogoEntidadEducativa.class);
+            listentidadeseducativas = q.getResultList();
+        }
+    }
+
+    public void onSelectListaCE() {
+        String listcodigos = "";
+        listrubroporce.clear();
+        listseleccionrubroporce.clear();
+
+        for (int i = 0; i < seleccionentidadEducativa.size(); i++) {
+            listcodigos = listcodigos + "," + "'" + seleccionentidadEducativa.get(i).getCodigoEntidad() + "'";
+        }
+
+        if (!seleccionentidadEducativa.isEmpty()) {
+            if (listcodigos.length() > 1) {
+                Query q;
+                q = em.createQuery("SELECT rpc "
+                        + "FROM RubroPorCe rpc "
+                        + "WHERE rpc.codigoEntidad.codigoEntidad in (" + listcodigos.substring(1) + ") "
+                        + "and rpc.estadoEliminacion = 0 ", RubroPorCe.class);
+                listrubroporce = q.getResultList();
+            }
+
+        }
+    }
+
+    public void onClickOption() {
+        codigoEntidad = "";
+        codigoRubro = "";
+        rubroporce = null;
+        listrubroporce.clear();
+        entidadEducativa = null;
+
+        listentidadeseducativas.clear();
+        listmunicipio.clear();
+        seleccionentidadEducativa.clear();
+        iddepartamento = "";
+        idmunicipio = "";
+        listseleccionrubroporce.clear();
+    }
+
+    public void guardar() {
+        if (tiposeleccion.equals("0")) {
+            guardarporce();
+        } else {
+            guardarporlista();
+        }
+
+    }
+
     public Boolean validarGuardar() {
         /*
         if (getProcesoAdquisicion() == null) {
             JsfUtil.mensajeAlerta("Debe de seleccionar un proceso de adquisición.");
             return false;
         }
-        */
+         */
         if (codigoEntidad == null) {
             JsfUtil.mensajeAlerta("Debe de seleccionar un centro educativo.");
             return false;
@@ -136,7 +273,7 @@ public class RubroPorCeView implements Serializable {
         return true;
     }
 
-    public void guardar() {
+    public void guardarporce() {
         if (validarGuardar()) {
             if (rubroporceRepo.existeRubro(codigoEntidad, Long.valueOf(codigoRubro))) {
                 JsfUtil.mensajeAlerta("El rubro seleccionado ya existe en la lista.");
@@ -156,6 +293,70 @@ public class RubroPorCeView implements Serializable {
         }
     }
 
+    public Boolean validarGuardarPorLista() {
+
+        if (codigoRubro == null) {
+            JsfUtil.mensajeAlerta("Debe de seleccionar un rubro a contratar.");
+            return false;
+        }
+        if (codigoRubro.isEmpty()) {
+            JsfUtil.mensajeAlerta("Debe de seleccionar un rubro a contratar.");
+            return false;
+        }
+        if (codigoRubro.equals("0")) {
+            JsfUtil.mensajeAlerta("Debe de seleccionar un rubro a contratar.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void guardarporlista() {
+        if (validarGuardarPorLista()) {
+            Boolean guardado = false;
+            String listcodigos = "";
+
+            for (int i = 0; i < seleccionentidadEducativa.size(); i++) {
+                listcodigos = listcodigos + "," + "'" + seleccionentidadEducativa.get(i).getCodigoEntidad() + "'";
+
+                if (rubroporceRepo.existeRubro(seleccionentidadEducativa.get(i).getCodigoEntidad(), Long.valueOf(codigoRubro))) {
+                    //JsfUtil.mensajeAlerta("El rubro seleccionado ya existe en la lista.");
+                } else {
+                    rubroporce = new RubroPorCe();
+                    rubroporce.setCodigoEntidad(seleccionentidadEducativa.get(i));
+                    rubroporce.setIdProcesoAdq(null);
+                    rubroporce.setIdRubroInteres(rubrosamostrarinteresrepo.listarrubroporid(codigoRubro));
+                    rubroporce.setFecha(LocalDate.now());
+                    rubroporce.setEstadoEliminacion(Boolean.FALSE);
+                    rubroporce.setUsuario(sessionView.getUsuario().getIdPersona().getUsuario());
+                    rubroporceRepo.save(rubroporce);
+                    rubroporce = null;
+
+                    guardado = true;
+
+                }
+            }
+
+            if (!seleccionentidadEducativa.isEmpty()) {
+                if (listcodigos.length() > 1) {
+                    Query q;
+                    q = em.createQuery("SELECT rpc "
+                            + "FROM RubroPorCe rpc "
+                            + "WHERE rpc.codigoEntidad.codigoEntidad in (" + listcodigos.substring(1) + ") "
+                            + "and rpc.estadoEliminacion = 0 ", RubroPorCe.class);
+                    listrubroporce = q.getResultList();
+                }
+
+            }
+
+            if (guardado) {
+                JsfUtil.mensajeInsert();
+            } else {
+                JsfUtil.mensajeAlerta("Proceso guardar finalizado con éxito. No se agregaron nuevos registros.");
+            }
+        }
+    }
+
     public void eliminar() {
         if (rubroporce != null) {
             if (rubroporce.getId() != null) {
@@ -163,11 +364,54 @@ public class RubroPorCeView implements Serializable {
                 rubroporce.setFecha(LocalDate.now());
                 rubroporce.setUsuario(sessionView.getUsuario().getIdPersona().getUsuario());
                 rubroporceRepo.update(rubroporce);
-                listrubroporce = rubroporceRepo.listarnoborradosporce(codigoEntidad); 
+                listrubroporce = rubroporceRepo.listarnoborradosporce(codigoEntidad);
                 JsfUtil.mensajeInformacion("El registro ha sido eliminado");
             } else {
                 JsfUtil.mensajeAlerta("Debe de seleccionar un registro a eliminar.");
             }
+        } else {
+            JsfUtil.mensajeAlerta("Debe de seleccionar un registro a eliminar.");
+        }
+
+    }
+
+    public void eliminarenlote() {
+        if (!listseleccionrubroporce.isEmpty()) {
+            for (int i = 0; i < listseleccionrubroporce.size(); i++) {
+                if (listseleccionrubroporce.get(i) != null) {
+                    if (listseleccionrubroporce.get(i).getId() != null) {
+                        listseleccionrubroporce.get(i).setEstadoEliminacion(Boolean.TRUE);
+                        listseleccionrubroporce.get(i).setFecha(LocalDate.now());
+                        listseleccionrubroporce.get(i).setUsuario(sessionView.getUsuario().getIdPersona().getUsuario());
+                        rubroporceRepo.update(listseleccionrubroporce.get(i));
+                        JsfUtil.mensajeInformacion("El registro ha sido eliminado");
+                    }
+                }
+            }
+
+            if (tiposeleccion.equals("0")) {
+                listrubroporce = rubroporceRepo.listarnoborradosporce(codigoEntidad);
+            } else {
+                String listcodigos = "";
+
+                for (int i = 0; i < seleccionentidadEducativa.size(); i++) {
+                    listcodigos = listcodigos + "," + "'" + seleccionentidadEducativa.get(i).getCodigoEntidad() + "'";
+                }
+
+                if (!seleccionentidadEducativa.isEmpty()) {
+                    if (listcodigos.length() > 1) {
+                        Query q;
+                        q = em.createQuery("SELECT rpc "
+                                + "FROM RubroPorCe rpc "
+                                + "WHERE rpc.codigoEntidad.codigoEntidad in (" + listcodigos.substring(1) + ") "
+                                + "and rpc.estadoEliminacion = 0 ", RubroPorCe.class);
+                        listrubroporce = q.getResultList();
+                    }
+
+                }
+
+            }
+
         } else {
             JsfUtil.mensajeAlerta("Debe de seleccionar un registro a eliminar.");
         }
